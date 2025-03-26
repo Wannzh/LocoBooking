47a3ce8d91453aab87b88b070b019cbc11afbacc
Muhamad Alwan Fadhlurrohman <alwanfdhlrhmn@gmail.com>
2025-03-26 20:47:32 +0700

    First Commit

---
+++ src/main/java/com/uas/locobooking/LocobookingApplication.java
@@ -0,0 +1,13 @@
+package com.uas.locobooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocobookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocobookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uas/locobooking/LocobookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uas.locobooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocobookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
